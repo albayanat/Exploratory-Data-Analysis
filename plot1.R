9ec5d4715bba477b04d1b891b5c737a628addc18
albayanat <44200307+albayanat@users.noreply.github.com>
2018-11-23 09:27:28 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,29 @@
+## plot1.R
+## Question: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+##Pre-requist: the file summarySCC_PM25.rds is in the working directory
+
+cat("Running script plot1.R... \n")
+
+## Read the required datafiles
+
+NEI <- readRDS("summarySCC_PM25.rds")
+
+
+## Compute the sum of Emissions of PM2.5 per year
+total_pm25_year <- tapply(NEI$Emissions, NEI$year, sum, na.rm = TRUE)
+
+## Open png device
+
+
+## close png device
+png("plot1.png", width=480, height=480)
+
+## Construct the plot
+
+barplot(TotalEmissionYear, main = "Total Emissions of PM2.5 per year", 
+        xlab = "Year", ylab = "Total Emissions of Pm2.5 (tons)")
+
+## Close the device
+dev.off()
+
+cat("plot1 saved into plot1.png \n End script plot1.R \n")

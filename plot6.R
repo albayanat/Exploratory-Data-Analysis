909fcbd0cec670d99fc4e0dff8b7c01338b12700
albayanat <44200307+albayanat@users.noreply.github.com>
2018-12-03 18:07:33 +0800

    Update plot6.R

--- plot6.R
+++ plot6.R
@@ -1,6 +1,7 @@
 ## plot6.R
 ## Question: Compare emissions from motor vehicle sources in Baltimore City with emissions from motor vehicle sources in Los Angeles County, California (fips=="06037"). 
 ## Which city has seen greater changes over time in motor vehicle emissions?
+##prerequisite: the files summarySCC_PM25.rds and Source_Classification_Code.rds are in the working directory
 
 cat("Running script plot6.R... \n")
 
@@ -16,20 +17,23 @@ SCC <- readRDS("Source_Classification_Code.rds")
 
 ## Subset to data related to motor vehicle sources (use SCC.Level.Two) in Baltimore and in Los Angeles
 SCC_vehicle<-subset(SCC, grepl("vehicle", SCC.Level.Two,ignore.case=TRUE ))
-NEI_BL_LA_vehicle<-subset(NEI, SCC %in% SCC_vehicle$SCC &  (fips ==  "24510"| fips =="06037"))
+NEI_BC_LA_vehicle<-subset(NEI, SCC %in% SCC_vehicle$SCC &  (fips ==  "24510"| fips =="06037"))
 
 ##Compute Total emissions of PM2.5 per year for motor vehicle sources
-df_BL_LA_vehicle_pm2.5<-ddply(NEI_BL_LA_vehicle, .(year, fips), function(x) sum(x$Emissions))
+df_BC_LA_vehicle_pm2.5<-ddply(NEI_BC_LA_vehicle, .(year, fips), function(x) sum(x$Emissions))
 
 ##Rearrange the dataframe
-colnames(df_BL_LA_vehicle_pm2.5)[3]<-"total_emissions"
-df_BL_LA_vehicle_pm2.5<- transform(df_BL_LA_vehicle_pm2.5, city = ifelse(fips == "24510", "Baltimore City", "Los Angeles County"))
+colnames(df_BC_LA_vehicle_pm2.5)[3]<-"total_emissions"
+df_BC_LA_vehicle_pm2.5<- transform(df_BC_LA_vehicle_pm2.5, city = ifelse(fips == "24510", "Baltimore City", "Los Angeles County"))
+
+##remove unused large objects
+rm(NEI,SCC, SCC_vehicle,NEI_BC_LA_vehicle)
 
 ## Open png device
 png("plot6.png", width=480, height=480)
 
 ## Construct the plot
-plot6<-ggplot(df_BL_LA_vehicle_pm2.5, aes(factor(year), total_emissions, fill = city))
+plot6<-ggplot(df_BC_LA_vehicle_pm2.5, aes(factor(year), total_emissions, fill = city))
 plot6<-plot6 + geom_bar(stat="identity")
 plot6<-plot6+facet_grid(.~city)
 plot6<-plot6+xlab("Year")+ylab("Total Emissions of PM2.5 (tons)")
